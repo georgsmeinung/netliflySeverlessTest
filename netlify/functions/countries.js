8e6a23d0fd291c2c01236c488eda76f8ff4531a1
Jorge Nicolau <jorge.nicolau@auth0.com>
2023-04-04 19:17:01 -0300

    Added Searching of Countries

---
+++ netlify/functions/countries.js
@@ -0,0 +1,24 @@
+exports.handler = async (event, context) => {
+    const COUNTRY = event.queryStringParameters.iso
+    const API_URL = 'https://restcountries.com/v3.1/alpha/'+COUNTRY
+    
+    if (!COUNTRY) {
+        return {
+        statusCode: 400,
+        body: JSON.stringify({
+          message: "ERROR: Parameter COUNTRY missing"
+        })
+    };
+    }
+    
+    const response = await fetch(API_URL)
+    const data = await response.json()
+    
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+          name: data[0].name.common,
+          flag: data[0].flags.png
+        })
+    };
+};
