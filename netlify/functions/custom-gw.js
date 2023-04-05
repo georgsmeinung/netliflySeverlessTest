98612c45f4a468b1da90f0d9a27362a40e8c5bbb
Jorge Nicolau <jorge.nicolau@auth0.com>
2023-04-05 13:20:42 -0300

    custom-gw request objects dumped to the console

--- netlify/functions/custom-gw.js
+++ netlify/functions/custom-gw.js
@@ -1,4 +1,9 @@
 exports.handler = async (event, context) => {
+    console.log("### CUSTOM GS ### "+(new Date()).toISOString)
+    console.log("### EVENT ###")
+    console.log(JSON.stringify(event))
+    console.log("### CONTEXT ###")
+    console.log(JSON.stringify(context))
     return {
         statusCode: 200,
         body: JSON.stringify({
