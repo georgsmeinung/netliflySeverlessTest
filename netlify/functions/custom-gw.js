3d8f8c3a19c96c506261b3f2de899f40650b4bbc
Jorge Nicolau <jorge.nicolau@auth0.com>
2023-04-05 14:26:38 -0300

    new body response

--- netlify/functions/custom-gw.js
+++ netlify/functions/custom-gw.js
@@ -1,13 +1,11 @@
 exports.handler = async (event, context) => {
-    var timestamp = new Date().toISOString;
-    console.log("### CUSTOM GS ### "+timestamp+" ###")
+    var opDate = new Date().toISOString;
+    console.log("### CUSTOM GS ### "+opDate+" ###")
     console.log(JSON.stringify(event.body))
     console.log("##################################################")
     return {
         statusCode: 200,
-        body: JSON.stringify({
-          currentDateTime: timestamp.toString(),
-          message: JSON.stringify(event.body)
+        body: JSON.stringify(event.body)
         })
     };
 };
